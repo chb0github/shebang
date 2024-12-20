fd1da5e2f7c6b46b37efb2f23510940b39b78042
Christian Bongiorno <chb0github@users.noreply.github.com>
2024-12-20 10:46:31 -0800

    Update assert.sh

--- assert.sh
+++ assert.sh
@@ -1,6 +1,6 @@
 function assert_file_not_empty() {
   local file=${1:?No file supplied}
-  assert "test $(du "${file}" | sed -En 's/([0-9]+).*/\1/p') -gt 0" "Your file '${file}' is empty"
+  assert "test -s '${file}'" "Your file '${file}' is empty"
 }
 
 function assert() {
@@ -8,4 +8,24 @@ function assert() {
   if ! eval "${1}"; then
     error "${1}: ${2}"
   fi
+}
+
+function assert_file_exists() {
+  assert "test -f ${1}" "${1} doesn't exist "
+}
+
+function assert_installed () {
+  local application="${1:?No application to assert supplied}"
+  assert "command -v ${application} >/dev/null" "you must install ${application}"
+}
+
+function assert_not_empty() {
+  local file=${1:?No file supplied}
+  
+}
+
+function assert_file_pattern() {
+  local pattern="${1:?you must supply a pattern}"
+  local msg="${2}"
+  assert "! grep -m 1 -qvE '${pattern}' '${pattern}'" "${msg}"
 }
