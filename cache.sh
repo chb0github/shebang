671db7e9e0e51412b6e8223b6089251ac38f7658
Christian Bongiorno <christian.bongiorno@gmail.com>
2025-02-06 14:40:30 -0800

    refactor the cache to accept a name

--- cache.sh
+++ cache.sh
@@ -1,9 +1,10 @@
 
 cache() {
-  local file
   local home=${HOME:-"$(echo ~)"}
-  test -d "$home/.cache/" || mkdir -p "$home/.cache/"
-  file="$HOME/.cache/bb/$(printf "%s" "${@}")"
+  local cache_dir="${home}/.cache/${1:?you must name your cache}"
+  local file
+  mkdir -p "${cache_dir}"
+  file="${cache_dir}/$(printf "%s" "${@}")"
   if test -f "${file}" && test $(($(date +%s) - $(date -r "$file" +%s))) -le 1800 ; then
     cat "${file}"
   else
